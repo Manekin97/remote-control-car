742c4aa4e9f8632cf9c909e53f401b06260bf7aa
Manekin97 <rafikdab@gmail.com>
2019-10-15 15:55:36 +0200

    Added joystick control

--- arduino_code/rc_car/rc_car.ino
+++ arduino_code/rc_car/rc_car.ino
@@ -13,10 +13,10 @@
 #define ECHO    12
 #define TRIGGER 13
 
-unsigned int distance;
-
 //  Servo motor pins
 #define SERVO_CONTROL 8
+
+unsigned int distance;
 
 Servo servo;
 int servoPosition = 90;
@@ -25,9 +25,15 @@ int change = 1;
 //  Defined as centimeters per microsecond to get the distance in centimeters
 const float SPEED_OF_SOUND = 0.034;
 
-int payload = 5;
+//  If an obstacle is closer than this distance (in centimeters), the car will try to evade it
+const float OBSTACLE_DISTANCE_THRESHOLD= 30;
+
+String command;
+int values[4];
+unsigned long timeOfStop = 0;
 
 SoftwareSerial ESP_serial(2, 3);  //  Tx, Rx
+
 enum CAR_ACTION {
   FORWARD,
   BACKWARD,
@@ -36,9 +42,15 @@ enum CAR_ACTION {
   STOP
 };
 
-unsigned long timeOfStop = 0;
+enum CONTROL_MODE {
+  REMOTE,
+  AUTONOMOUS
+};
+
+CONTROL_MODE controlMode = REMOTE;
 
 void setup() {
+  //  Setup serial connection
   ESP_serial.begin(9600);
   Serial.begin(115200);
 
@@ -55,55 +67,114 @@ void setup() {
   pinMode(ECHO, INPUT);
 
   //  Attach servo control pin to servo
-  servo.attach(SERVO_CONTROL);
-
-  //  Setup timer interrupt
-  
-   noInterrupts();
-   
-  // Set compare match register for 100Hz increments
-  OCR2A = 156;
-  // turn on CTC mode
-  TCCR2B |= (1 << WGM12);
-  // Set prescaler to 1024
-  TCCR2B |= (1 << CS12) | (1 << CS10);
-  // Enable timer compare interrupt
-  TIMSK2 |= (1 << OCIE2A);
-
-  interrupts();
-}
-
-ISR(TIMER2_COMPA_vect){
-  distance = readSensor();
+//  servo.attach(SERVO_CONTROL);
 }
 
 void loop() {
-  scanForObstacles();
-  
   if (ESP_serial.available() > 0) {
-    char payload_raw = ESP_serial.read();
-    payload = (int)payload_raw - 48;
+    char character = ESP_serial.read();
+    
+    command += character;
+    if (character == '\n') {
+      getValuesFromString(command, values);
+      command = "";
+      
+      handleCommands(values[0], values[1], values[2], values[3]);
+    }
+
     timeOfStop = millis();
   }
 
-  switch (payload) {
-    case 0:
-      move(BACKWARD, 215);
-      break;
-    case 1:
-      move(RIGHT, 215);
-      break;
-    case 2:
-      move(LEFT, 215);
-      break;
-    case 3:
-      move(FORWARD, 215);
-      break;
-    default:
-      if (millis() - timeOfStop > 100){
-          move(STOP, 215);
+  if (controlMode == AUTONOMOUS) {
+    move(FORWARD, 255);
+    servo.write(90);
+    distance = readSensor();
+
+    if(distance < OBSTACLE_DISTANCE_THRESHOLD) {
+      servo.write(180);
+      distance = readSensor();
+
+      if (distance < OBSTACLE_DISTANCE_THRESHOLD) {
+        servo.write(0);
+        distance = readSensor();
+        
+        if(distance < OBSTACLE_DISTANCE_THRESHOLD) {
+          move(BACKWARD, 255);
+          delay(1000);
+        } else {
+          move(RIGHT, 255);
+          delay(300);
+        }
+      } else {
+        move(LEFT, 255);
+        delay(300);
+      }
+    }
+  }
+}
+
+void handleCommands(int left, int right, int dir, int mode) {
+  if (dir) {
+    digitalWrite(LEFT_MOTOR_IN_1, LOW);
+    digitalWrite(LEFT_MOTOR_IN_2, HIGH);
+    digitalWrite(RIGHT_MOTOR_IN_3, LOW);
+    digitalWrite(RIGHT_MOTOR_IN_4, HIGH);
+  } else {
+    digitalWrite(LEFT_MOTOR_IN_1, HIGH);
+    digitalWrite(LEFT_MOTOR_IN_2, LOW);
+    digitalWrite(RIGHT_MOTOR_IN_3, HIGH);
+    digitalWrite(RIGHT_MOTOR_IN_4, LOW);
+  }
+
+  if (mode) {
+    setMode(AUTONOMOUS);
+  } else {
+    setMode(REMOTE);
+  }
+
+  analogWrite(ENABLE_LEFT_MOTOR, left);
+  analogWrite(ENABLE_RIGHT_MOTOR, right);
+}
+
+void getValuesFromString(String string, int *buff) {
+  int len = string.length();
+  int valueIndex = 0;
+  int beginningIndex = 0;
+  
+  for (int i  = 0; i < len; i++) {
+    if (string[i] == ' ') {
+      String substr = string.substring(beginningIndex, i);
+      buff[valueIndex++] = substr.toInt();
+      beginningIndex = i + 1;
+    }
+  }
+}
+
+void handleCommand(int commandCode) {
+  if(controlMode == REMOTE) {
+    switch (commandCode) {
+      case 0:
+        move(BACKWARD, 255);
+        break;
+      case 1:
+        move(RIGHT, 255);
+        break;
+      case 2:
+        move(LEFT, 255);
+        break;
+      case 3:
+        move(FORWARD, 255);
+        break;
+      default:
+        if (millis() - timeOfStop > 100){
+            move(STOP, 215);
+        }
       }
   }
+}
+
+void setControlMode(CONTROL_MODE mode) {
+  controlMode = mode;
 }
 
 void scanForObstacles() {
@@ -143,8 +214,8 @@ void move(CAR_ACTION action, int speed) {
       digitalWrite(RIGHT_MOTOR_IN_3, LOW);
       digitalWrite(RIGHT_MOTOR_IN_4, HIGH);
 
-      analogWrite(ENABLE_LEFT_MOTOR, speed);
-      analogWrite(ENABLE_RIGHT_MOTOR, speed * 0.75);
+      analogWrite(ENABLE_LEFT_MOTOR, 0);
+      analogWrite(ENABLE_RIGHT_MOTOR, speed);
       break;
     case RIGHT:
       digitalWrite(LEFT_MOTOR_IN_1, LOW);
@@ -152,8 +223,8 @@ void move(CAR_ACTION action, int speed) {
       digitalWrite(RIGHT_MOTOR_IN_3, LOW);
       digitalWrite(RIGHT_MOTOR_IN_4, LOW);
 
-      analogWrite(ENABLE_LEFT_MOTOR, speed * 0.75);
-      analogWrite(ENABLE_RIGHT_MOTOR, speed);
+      analogWrite(ENABLE_LEFT_MOTOR, speed);
+      analogWrite(ENABLE_RIGHT_MOTOR, 0);
       break;
     case STOP:
       digitalWrite(LEFT_MOTOR_IN_1, LOW);
@@ -183,5 +254,6 @@ unsigned int readSensor() {
   // Calculate the distance
   unsigned int dist = duration * SPEED_OF_SOUND / 2;
 
+  Serial.println(dist);
   return dist;
 }
--- esp8266_code/esp8266_code.ino
+++ esp8266_code/esp8266_code.ino
@@ -1,54 +1,55 @@
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
+#include <ArduinoJson.h>
 
-//const char* ssid = "TP-Link_2.4";
-//const char* password = "1997rafal1997";
-const char* ssid = "mordor";
-const char* password = "adminadmin";
+const char* ACCESS_POINT_NAME = "ESP8266-ACCESS-POINT";
+
+IPAddress IP(192,168,1,1);
+IPAddress GATEWAY(192,168,1,1);
+IPAddress SUBNET(255,255,255,0);
 
 WiFiUDP Udp;
 unsigned int localUdpPort = 4210;
-char incomingPacket[256];
-char replyPacket[] = "ok.";
+const size_t jsonSize = JSON_OBJECT_SIZE(4) + 70;
+char incomingPacket[jsonSize];
+DynamicJsonDocument json(jsonSize);
 
 void setup() {
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+  
   Serial.begin(9600);
   Serial.println();
-
-  Serial.printf("Connecting to %s ", ssid);
-  WiFi.begin(ssid, password);
+  Serial.printf("Creating %s ", ACCESS_POINT_NAME);
   
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  
-  Serial.println(" connected");
+  WiFi.mode(WIFI_AP);
+  WiFi.softAPConfig(IP, GATEWAY, SUBNET);
+  boolean result = WiFi.softAP(ACCESS_POINT_NAME, "", 13, false, 1);
+  Serial.println(result ? "Access Point is ready" : "Setting up Access Point failed");
 
   Udp.begin(localUdpPort);
-  Serial.printf("Now listening at IP %s, UDP port %d\n", WiFi.localIP().toString().c_str(), localUdpPort);
+  Serial.printf("Access Point is ready at IP %s, UDP port %d\n", WiFi.softAPIP().toString().c_str(), localUdpPort);
 }
 
 void loop() {
- int packetSize = Udp.parsePacket();
+  int packetSize = Udp.parsePacket();
  
   if (packetSize) {
     // receive incoming UDP packets
     int len = Udp.read(incomingPacket, 255);
     if (len > 0) {
-      incomingPacket[len] = '\0';
-
-      Serial.printf("%d", incomingPacket[0]);
-    }
-
-    // send back a reply, to the IP address and port we got the packet from
-    Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
-    Udp.write(replyPacket);
-    Udp.endPacket();
-  }
-  else {
-//      Serial.printf("%d\n", -1);
-    }
+      DeserializationError error = deserializeJson(json, incomingPacket);
   
-
+      if (!error) {
+        int left_motor_speed = json["left_motor_speed"];
+        int right_motor_speed = json["right_motor_speed"];
+        int dir = json["direction"];
+        int driving_mode = json["driving_mode"];
+        Serial.printf("%d %d %d %d\n", left_motor_speed, right_motor_speed, dir, driving_mode);
+      } else {
+        Serial.printf("%s\n", error.c_str());
+      }
+    }
+  }
 }
